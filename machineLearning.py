a2a4a0b2e44af36977e0aa4528963667254714fb
VersaceSauce <chchung@tesla.com>
2018-02-01 17:53:48 -0800

    Added MCU board.

--- machineLearning.py
+++ machineLearning.py
@@ -6,12 +6,12 @@ import numpy as np
 import seaborn as sns
 from sklearn import svm
 
-df = pd.read_csv('Isowrist_biceps.csv')
-df.columns = ['X3','X2','X1', 'Y']
-#df = df.drop(['X4','X3'], 1)
+df = pd.read_csv('ArmFlection.csv')
+df.columns = ['X4','X3','X2','X1', 'Y']
+df = df.drop(['X4','X1'], 1)
 df.head()
-X = df.values[:, 0:3]
-Y = df.values[:, 3]
+X = df.values[:, 0:2]
+Y = df.values[:, 2]
 
 from sklearn.model_selection import train_test_split
 
